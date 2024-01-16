fb3a6b3c0ba81fd2c9423f49c5ce27e78c9a8f7e
olidec <69003898+olidec@users.noreply.github.com>
2024-01-16 22:50:48 +0100

    added exercise branch

--- src/main.js
+++ src/main.js
@@ -4,17 +4,23 @@ function generateBookList(data) {
     
     // Create an empty string to store HTML content
     let htmlContent = '';
+
+    
     
     // Use a for loop to iterate through the books array
     for (let i = 0; i < books.length; i++) {
         const book = books[i];
+        let yearClass = 'year'
+        if (book.year < 1945) {
+            yearClass += ' old';
+        }
         
         // Append HTML content for each book
         htmlContent += `
-        <div>
+        <div class="book">
         <h2>${book.title}</h2>
         <p><strong>Author:</strong> ${book.author}</p>
-        <p><strong>Year:</strong> ${book.year}</p>
+        <p class="${yearClass}"><strong>Year:</strong> ${book.year}</p>
         </div>
         `;
     }
@@ -23,4 +29,5 @@ function generateBookList(data) {
     bookListElement.innerHTML = htmlContent;
 }
 
+// Load JSON data and then generate the book list
 loadJSON('public/books.json').then(generateBookList);
